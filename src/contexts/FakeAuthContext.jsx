468d967311c01763de227f56c7fede890438ea09
badal <badal@badals-MacBook-Air.local>
2023-12-10 23:35:51 +0530

    final

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -19,8 +19,8 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
-  email: "jack@example.com",
+  name: "Badal",
+  email: "Badal@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
 };
